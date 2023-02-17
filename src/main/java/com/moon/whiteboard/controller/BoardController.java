cce3ad7564532009e0abf33bd82b3c2d26ea3bf7
SeongSiHun <gjsdlfkddmsd@gmail.com>
2023-02-17 23:04:54 +0900

    BoardWrite home에서 이동버튼 추가

--- src/main/java/com/moon/whiteboard/controller/BoardController.java
+++ src/main/java/com/moon/whiteboard/controller/BoardController.java
@@ -3,7 +3,9 @@ package com.moon.whiteboard.controller;
 import com.moon.whiteboard.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/board/*")
@@ -12,5 +14,10 @@ public class BoardController {
     @Autowired
     BoardService boardService;
 
+    @GetMapping("/write")
+    public ModelAndView getWrite(ModelAndView mv){
+        mv.setViewName("/board/boardWrite");
+        return mv;
+    }
 
 }
--- src/main/java/com/moon/whiteboard/serviceImpl/BoardServiceImpl.java
+++ src/main/java/com/moon/whiteboard/serviceImpl/BoardServiceImpl.java
@@ -12,4 +12,5 @@ public class BoardServiceImpl implements BoardService {
 
     @Autowired
     BoardMapper boardMapper;
+
 }
